441a05b3798bbf3385e98aefd2296484a99c2d7f
AlexChow <cplover@live.com>
2015-10-24 21:52:06 +0800

    replace ip with domain

--- AppTableInitInsert.sql
+++ AppTableInitInsert.sql
@@ -7,8 +7,9 @@ LIMIT 0, 1000
 USE BahamutDB;
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (7,'0abb007ad297bc4f5ce75089dc186490c2211f07','MySQL',NULL,NULL,'2015-10-23 14:55:04');
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (8,'0c4618e43e8e05a742036e070e4ab3d3c501b73d','Redis',NULL,NULL,'2015-10-23 14:55:04');
+INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (9,'e19b471a1370ccc4b12673469516dbd70b90d5c1','Mongodb',NULL,NULL,'2015-10-23 14:55:04');
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (10,'5e6c827f2fcb04e8fca80cf72db5ba004508246b','Toronto',NULL,NULL,'2015-10-23 14:55:04');
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (11,'7a02f76c05e2882f0bd72e9c1e03bafcab74a869','BahamutFireFileServer',NULL,NULL,'2015-10-23 14:55:04');
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (12,'b078082bd9d5c63da1b4c5d546a9fa44b6c879b2','BahamutFireAPIServer',NULL,NULL,'2015-10-23 14:55:04');
-INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (9,'e19b471a1370ccc4b12673469516dbd70b90d5c1','Mongodb',NULL,NULL,'2015-10-23 14:55:04');
 INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (13,'e1c91545bdb803aaeba2f5d4c86308327b93a2e2','Authentication',NULL,NULL,'2015-10-23 14:55:04');
+INSERT INTO `App` (`Id`,`Appkey`,`Name`,`Document`,`Description`,`CreateTime`) VALUES (14,'851753bffd33391e5db003ed2845016f2021b497','Chicago',NULL,NULL,'2015-10-24 21:24:04');
