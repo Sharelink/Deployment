6c9d84b3e85f87919a51dc95ad19e4bdd507e7de
AlexChow <cplover@live.com>
2015-11-26 18:06:19 +0800

    update

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -1,3 +1,7 @@
 #init server
 groupadd deployment
 useradd deployment -m -d /home/deployment -g deployment
+
+#set mono env
+echo '#set mono thread env’>>/etc/profile
+echo 'export MONO_THREADS_PER_CPU=2000'>>/etc/profile
