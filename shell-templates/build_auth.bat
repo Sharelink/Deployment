d1586f977554db80a633b9f33188ac80f7f012a3
alexchow <cplover@live.com>
2016-10-19 12:47:50 +0800

    update

--- shell-templates/build_auth.bat
+++ shell-templates/build_auth.bat
@@ -1,4 +1,5 @@
 cd D:\Source\Git\bahamut\AuthenticationServer\src\AuthenticationServer
 D:
+dotnet restore
 dotnet publish -f netcoreapp1.0 -r ubuntu.14.04-x64 -c Release -o D:\Source\Git\bahamut\Deployment\apps\Auth
 pause
--- shell-templates/build_chicago.bat
+++ shell-templates/build_chicago.bat
@@ -1,4 +1,5 @@
 cd D:\Source\Git\bahamut\Chicago\src\Chicago
 D:
+dotnet restore
 dotnet publish -f netcoreapp1.0 -r ubuntu.14.04-x64 -c Release -o D:\Source\Git\bahamut\Deployment\apps\Chicago
 pause
